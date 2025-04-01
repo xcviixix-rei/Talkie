be01f98848db41f8b01af67e53af7fba1eaafbd9
xcviixix-rei <124432779+xcviixix-rei@users.noreply.github.com>
2025-04-01 21:58:39 +0700

    Create Call API Key

--- frontend/app/callscreen.js
+++ frontend/app/callscreen.js
@@ -3,8 +3,9 @@ import { SafeAreaView, StyleSheet } from "react-native"
 import { StreamVideo, StreamVideoClient } from "@stream-io/video-react-native-sdk"
 import { ActiveCall } from "./call/activecall"
 import { InactiveCall } from "./call/inactivecall"
+import { GETSTREAM_API_KEY } from "@env"
 
-const apiKey = "mmhfdzb5evj2"
+const apiKey = GETSTREAM_API_KEY
 const token =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0NvdW50X0Rvb2t1IiwidXNlcl9pZCI6IkNvdW50X0Rvb2t1IiwidmFsaWRpdHlfaW5fc2Vjb25kcyI6NjA0ODAwLCJpYXQiOjE3NDI3Mzg0MTUsImV4cCI6MTc0MzM0MzIxNX0.y2s56Rgd1C2hv3P1r_IFIugM3Iu6zZUPGId1d4YF90A"
 const userId = "Count_Dooku"
