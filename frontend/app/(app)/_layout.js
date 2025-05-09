6a98ed8ed9fb5b8177bfe11ab02bbf104ed1c51a
Raki <quangdatltw@gmail.com>
2025-05-09 23:07:19 +0700

    Fix merge error

--- frontend/app/(app)/_layout.js
+++ frontend/app/(app)/_layout.js
@@ -1,63 +1,64 @@
 import {Stack, Tabs} from "expo-router";
 
 export default function AppLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen
-        name="search"
-        options={{
-          title: "Search",
-          headerBackTitle: "Home",
-        }}
-      />
-      <Tabs.Screen
-        name="createGroup"
-        options={{
-          title: "Create Group",
-        }}
-      />
-      <Stack.Screen name="conversation" />
-      <Stack.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-          headerBackTitle: "Back",
-        }}
-      />
-      <Stack.Screen
-        name="callScreen"
-        options={{
-          headerShown: false,
-          presentation: "fullScreenModal",
-        }}
-      />
-      <Stack.Screen
-        name="converInfor"
-        options={{
-          headerShown: false,
-        }}
-      />
-      <Stack.Screen
-        name="notifications-settings"
-        options={{
-          headerShown: false,
-          headerBackTitle: "Back",
-        }}
-      />
-      <Stack.Screen
-        name="settings"
-        options={{
-          headerShown: false,
-          headerBackTitle: "Back",
-        }}
-      />
+    return (
+        <Stack>
+            <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
+            <Stack.Screen
+                name="search"
+                options={{
+                    title: "Search",
+                    headerBackTitle: "Home",
+                }}
+            />
+            <Tabs.Screen
+                name="createGroup"
+                options={{
+                    title: "Create Group",
+                }}
+            />
+            <Stack.Screen name="conversation"/>
+            <Stack.Screen
+                name="profile"
+                options={{
+                    title: "Profile",
+                    headerBackTitle: "Back",
+                }}
+            />
+            <Stack.Screen
+                name="callScreen"
+                options={{
+                    headerShown: false,
+                    presentation: "fullScreenModal",
+                }}
+            />
+            <Stack.Screen
+                name="converInfor"
+                options={{
+                    headerShown: false,
+                }}
+            />
+            <Stack.Screen
+                name="notifications-settings"
+                options={{
+                    headerShown: false,
+                    headerBackTitle: "Back",
+                }}
+            />
+            <Stack.Screen
+                name="settings"
+                options={{
+                    headerShown: false,
+                    headerBackTitle: "Back",
+                }}
+            />
             <Stack.Screen
                 name="collection"
                 options={{
                     headerBackTitle: "Back",
-                }}
                     headerShown: false,
+                }}
+
             />
             <Stack.Screen
                 name="collectionInfo"
@@ -66,12 +67,14 @@ export default function AppLayout() {
                     headerBackTitle: "Back",
                 }}
             />
-                name="createCollection"
+
             <Stack.Screen
+                name="createCollection"
                 options={{
                     headerShown: false,
-                }}
                     headerBackTitle: "Back",
+                }}
+
             />
         </Stack>
     );
